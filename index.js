9cb5d7acd67af4c0c81b7d8208a0f06dbdb4f00c
space doge <99536044+d0g3bl2@users.noreply.github.com>
2022-11-06 15:14:06 +0000

    add stratUp func .js

--- index.js
+++ index.js
@@ -9,3 +9,22 @@ function drawBox(container, row, column, letter = '') {
     
     return box;
 }
+
+function drawGrid(container) {
+    const grid = document.createElement('div');
+    grid.className = 'grid';
+
+    for (let i = 0; i < 6; i++) {
+        for (let j = 0; j < 5; j++) {
+            drawBox(grid, i, j)
+            
+        }
+    }
+
+    container.appendChild(grid);
+}
+
+function startUp() {
+    const game = document.querySelector('#game');
+    drawGrid(game);
+}
