8dffa77755cc9148ea1060a1ba481c34e56672ef
space doge <99536044+d0g3bl2@users.noreply.github.com>
2022-11-07 14:42:36 +0000

    add secret prop to state obj .js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const dictionary = ['tears', 'water', 'river', 'juice'];
 
 const state = {
+    secret: dictionary[Math.floor(Math.random() * dictionary.length)],
     grid: Array(6).fill().map(() => Array(5).fill('')),
     currentRow: 0,
     currentColumn: 0,
